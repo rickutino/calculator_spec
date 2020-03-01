6c78ead076053a8e9373234f0c443d4356a0df0c
rickutino <rickutino@gmail.com>
2020-03-02 00:43:33 +0900

    Implicit Subject

--- example_rspec_tdd/spec/calculator/calculator_spec.rb
+++ example_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,22 +1,19 @@
 require 'Calculator'
 
-describe Calculator do
-
-  subject(:calc) { described_class.new() }
-
+describe Calculator, "about the calculator" do
   context '#sum' do 
     it 'with positive numbers' do
-      result = calc.sum(5,5)
+      result = subject.sum(5,5)
       expect(result).to eq(10)
     end
 
     it 'with negative and poditive numbers' do
-      result = calc.sum(-5,15)
+      result = subject.sum(-5,15)
       expect(result).to eq(10)
     end
 
     it 'with negative numbers' do
-      result = calc.sum(-5,-8)
+      result = subject.sum(-5,-8)
       expect(result).to eq(-13)
     end
   end
