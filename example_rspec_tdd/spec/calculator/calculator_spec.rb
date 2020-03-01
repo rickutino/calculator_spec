c8ebbf08f30f5bc3b4e9e1072ca3f09f608faea8
rickutino <rickutino@gmail.com>
2020-03-02 00:11:48 +0900

    Subject

--- example_rspec_tdd/spec/calculator/calculator_spec.rb
+++ example_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,22 +1,21 @@
 require 'Calculator'
 
 describe Calculator do
-  context '#sum' do #instace name add #  Class name add . for inicial context
-    it 'with positive numbers' do  #xUnit
-      calc = Calculator.new               #Setup
-      result = calc.sum(5,5)              #Exercise
-      expect(result).to eq(10)            #Verify
-                                          #Teardown
+
+  subject(:calc) { described_class.new() }
+
+  context '#sum' do 
+    it 'with positive numbers' do
+      result = calc.sum(5,5)
+      expect(result).to eq(10)
     end
 
     it 'with negative and poditive numbers' do
-      calc = Calculator.new
       result = calc.sum(-5,15)
       expect(result).to eq(10)
     end
 
     it 'with negative numbers' do
-      calc = Calculator.new
       result = calc.sum(-5,-8)
       expect(result).to eq(-13)
     end
